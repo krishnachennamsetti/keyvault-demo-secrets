04ca386294f3c5090a2185e65cbc11cc9073ea0d
Krishna Chennamsetti <krishna.chennamsetti@gmail.com>
2023-04-16 11:16:23 +0200

    keyvault fetch secrets changes

--- src/main/java/com/azure/sample/keyvault/controller/KeyvaultController.java
+++ src/main/java/com/azure/sample/keyvault/controller/KeyvaultController.java
@@ -5,14 +5,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/v1/secrets")
 public class KeyvaultController {
+    private final String keyVaultUri ="https://mysamplekeyvault.vault.azure.net/";
 
     @GetMapping(value = "/fetch")
     public ResponseEntity<String> fetchSecrets() {
-        String secret = "hello i'm secret";
+        SecretClient secretClient = new SecretClientBuilder()
+                .vaultUrl(keyVaultUri)
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+        String secret = Objects.nonNull(secretClient) ? secretClient.getSecret("mysecret").toString() : "No secret found";
         return new ResponseEntity<>(secret, HttpStatus.OK);
     }
 
